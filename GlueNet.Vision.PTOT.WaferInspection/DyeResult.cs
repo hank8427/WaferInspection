191e487a6d2c60b31b2322baac51d45036c0a499
Hank <hank.you@glue-soft.com>
2025-06-09 16:36:27 +0800

    Revise properties of DyeResult class

--- GlueNet.Vision.PTOT.WaferInspection/DyeResult.cs
+++ GlueNet.Vision.PTOT.WaferInspection/DyeResult.cs
@@ -9,9 +9,9 @@ namespace GlueNet.Vision.PTOT.WaferInspection
     public class DyeResult
     {
         public string Name { get; set; }
-        public int Row { get; set; }
-        public int Column { get; set; }
         public int Section { get; set; }
+        public int Column { get; set; }
+        public int Row { get; set; }
         public string OKNG { get; set; }
         public string AiDetectResult { get; set; }
     }
